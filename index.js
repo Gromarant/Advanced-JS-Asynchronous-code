da0ae95b4f1dd4b12ea2fe98f7ce14c047893754
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-10 19:33:11 +0200

    Solve exercise 1 of advance-js-asynchronous-code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//RESUELVE TUS EJERCICIOS AQUI
+/*Dog API - Quiero un perrito, pero no se qué raza escoger, ¿me ayudas?
+1.- Declara una funcion getAllBreeds que devuelva todas las razas de perro.
+*/
+const getAllBreeds = () => {
+  let arr = fetch("https://dog.ceo/api/breeds/list/all")
+    .then((res) => res.json())
+    .then((result) => {
+      let allBreeds = [];
+      for (let breed in result['message']) {
+        allBreeds.push(breed);
+      }
+      return allBreeds;
+    });
+  return arr;
+};
